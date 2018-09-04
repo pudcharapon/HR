e1384870d12dec1c45776a9bd723c3ecd0bba268
pudcharapon <golf6224471@gmail.com>
2018-09-04 15:47:17 +0700

    commit qw2

--- testfunction/src/testfunction/mainFunction.java
+++ testfunction/src/testfunction/mainFunction.java
@@ -32,6 +32,7 @@ public class mainFunction {
 		System.out.println("d");
 		System.out.println("x");
 		System.out.println("qw");
+		System.out.println("qw2");
 	person firstemployee = new person();
 	firstemployee.generateID();
 	}
